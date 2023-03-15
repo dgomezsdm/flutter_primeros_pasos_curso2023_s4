543e83e2c6d24f544c673c4238b98600e9e79a1e
neryad <dayern.gomez@hotmail>
2023-03-15 15:17:18 -0400

    app complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_primeros_pasos_curso2023_s4/src/presetation/screens/counter/counter_screen.dart';
+
+import 'src/presentation/screens/counter_functions_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,6 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
-        home: const CounterScreen());
+        home: const CounterFunctionScreen());
   }
 }
